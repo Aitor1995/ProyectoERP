0ce1e7bd2ad463b713b57753b63a9d4713101156
guillegonrod <guillegonrod@hotmail.com>
2015-02-03 09:45:00 +0100

    Terminado __openerp__

---
+++ __openerp__.py
@@ -0,0 +1,16 @@
+# -*-coding:utf-8-*-
+{
+    "name": "Pokemon",
+    "version": "1.0",
+    "description": "Universo Pokemon",
+    "author": "Aitor&Guille",
+    "category": "Videogames",
+    "depends": [
+        "base",
+    ],
+    "data": [
+        "pokemon_view.xml"
+    ],
+    "active": False,
+    "installable": True,
+}
